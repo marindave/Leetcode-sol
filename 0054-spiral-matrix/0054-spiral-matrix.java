8ea2f9bc5b4610d97cdddc76bead1a00015e97e8
marindave <marindave856@gmail.com>
2025-04-06 14:45:30 +0530

    Time: 0 ms (100%), Space: 41.8 MB (26.56%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.java
+++ 0054-spiral-matrix/0054-spiral-matrix.java
@@ -1,42 +1,38 @@
 class Solution {
     public List<Integer> spiralOrder(int[][] matrix) {
-        int n = matrix.length; // Number of rows
-        int m = matrix[0].length; // Number of columns
-        int left = 0, right = m - 1;
-        int top = 0, bottom = n - 1;
-
-        ArrayList<Integer> list = new ArrayList<>();
-
-        while (left <= right && top <= bottom) {
-            // Traverse from left to right
-            for (int i = left; i <= right; i++) {
-                list.add(matrix[top][i]);
+       List <Integer> ans =new ArrayList<>();
+       int n=matrix.length;
+       int m=matrix[0].length;
+       int sr=0;
+        int sc=0;
+        int er=n-1;
+        int ec=m-1;
+        while(sr<=er && sc<=ec){
+            for(int j=sc;j<=ec;j++){
+                ans.add(matrix[sr][j]);
+               
             }
-            top++;
-
-            // Traverse from top to bottom
-            for (int i = top; i <= bottom; i++) {
-                list.add(matrix[i][right]);
+             sr++;
+            for(int i=sr;i<=er;i++){
+                ans.add(matrix[i][ec]);
+               
             }
-            right--;
-
-            // Traverse from right to left
-            if (top <= bottom) {
-                for (int i = right; i >= left; i--) {
-                    list.add(matrix[bottom][i]);
-                }
-                bottom--;
+             ec--;
+            if(sr<=er){
+                for(int j=ec;j>=sc;j--){
+                ans.add(matrix[er][j]);
+                
             }
-
-            // Traverse from bottom to top
-            if (left <= right) {
-                for (int i = bottom; i >= top; i--) {
-                    list.add(matrix[i][left]);
-                }
-                left++;
             }
+            er--;
+            if(sc<=ec){
+                for(int i=er;i>=sr;i--){
+                ans.add(matrix[i][sc]);
+                
+            }
+            }
+            sc++;
         }
-
-        return list;
+        return ans;
     }
 }
