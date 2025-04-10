c050985dca1f3c2178150eb771e7b8d73fdc0daa
marindave <marindave856@gmail.com>
2025-04-10 15:33:46 +0530

    Time: 2 ms (7.68%), Space: 45.5 MB (44.96%) - LeetHub

--- 2048-build-array-from-permutation/2048-build-array-from-permutation.java
+++ 2048-build-array-from-permutation/2048-build-array-from-permutation.java
@@ -1,10 +1,16 @@
 class Solution {
     public int[] buildArray(int[] nums) {
+        
         int n=nums.length;
-        int[] ans=new int[n];
+    
         for(int i=0;i<n;i++){
-            ans[i]=nums[nums[i]];
+            nums[i]=nums[i]+n*(nums[nums[i]]%n);//encoding new and current value
         }
-        return ans;
+        for(int i=0;i<n;i++){
+            nums[i]=nums[i]/n;//extracting new value
+        }
+        return nums;
     }
 }
+
+    
