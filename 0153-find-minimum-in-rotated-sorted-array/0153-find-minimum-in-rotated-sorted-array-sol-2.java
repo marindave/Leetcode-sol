9f14b23f72eaa25f6ff86d7efb4ff00bba065a34
marindave <marindave856@gmail.com>
2025-04-13 08:29:41 +0530

    Create 0153-find-minimum-in-rotated-sorted-array-sol-2.java

---
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array-sol-2.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int findMin(int[] nums) {
+      int n=nums.length;
+        int start=0;
+        int end=n-1;
+       //this code actually find the minima and all the two pointers will point to minima
+          
+        while(start<end){
+            
+            int mid=start+(end-start)/2;
+           if(nums[mid]>nums[end]){
+            //which means minimum will lie on the right side of mid
+            start=mid+1;
+           }
+          else{
+            //which means minimum will lie on the left side of mid including mid also
+            end=mid;
+          }
+
+    }
+    return nums[start];
+    }
+   
+}
