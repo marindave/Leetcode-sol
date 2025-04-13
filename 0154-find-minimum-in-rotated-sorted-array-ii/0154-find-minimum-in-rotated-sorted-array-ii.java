2b806f7774e07284b7f8ef670aeea46a27a47018
marindave <marindave856@gmail.com>
2025-04-13 09:45:07 +0530

    Time: 0 ms (100%), Space: 43.7 MB (8.34%) - LeetHub

--- 0154-find-minimum-in-rotated-sorted-array-ii/0154-find-minimum-in-rotated-sorted-array-ii.java
+++ 0154-find-minimum-in-rotated-sorted-array-ii/0154-find-minimum-in-rotated-sorted-array-ii.java
@@ -1,43 +1,30 @@
 class Solution {
     public int findMin(int[] nums) {
-        //Removing the adjacent dupicate elements from array
-        //converting the given array nums into list
-        List<Integer> ans=new ArrayList<>();
-        ans.add(nums[0]);
-        for(int i=1;i<nums.length;i++){
-            if(nums[i]!=nums[i-1]){
-                ans.add(nums[i]);
+      int n=nums.length;
+        int start=0;
+        int end=n-1;
+       //this code actually find the minima and all the two pointers will point to minima
+          
+        while(start<end){
+            
+            int mid=start+(end-start)/2;
+            if(nums[mid]==nums[start]&&nums[mid]==nums[end]){
+                start++;
+                end--;
+                
             }
-        }
-        //converting the list ans into array
-        int[] result=new int[ans.size()];
-        for(int i=0;i<ans.size();i++){
-            result[i]=ans.get(i);
-        }
-        int pivot=findPivot(result);
-         if(pivot==-1){
-         return result[0];
-     }
-        return result[pivot+1];
-     
-    }
-   public static int findPivot(int[] nums){
-    int start=0;
-    int end=nums.length-1;
-    while(start<=end){
-        int mid=start+(end-start)/2;
-        if(mid<end&&nums[mid]>nums[mid+1]){
-            return mid;
-        }
-        if(mid>start&&nums[mid]<nums[mid-1]){
-            return mid-1;
-        }
-        if(nums[start]>=nums[mid]){
-            end=mid-1;
-        }else{
+            
+         else  if(nums[mid]>nums[end]){
+            //which means minimum will lie on the right side of mid
             start=mid+1;
-        }
+           }
+          else{
+            //which means minimum will lie on the left side of mid including mid also
+            end=mid;
+          }
+
     }
-    return -1;
-}
+    return nums[start];
+    }
+   
 }
